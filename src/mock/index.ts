87334f70ff4910f6239c0ae71b9f39cdcb396b37
fedmike <molnek87@mail.ru>
2025-11-13 23:40:58 +0900

    stat component done

---
+++ src/mock/index.ts
@@ -0,0 +1,17 @@
+import { LocalGithubUser } from "types";
+
+export const defaultUser: LocalGithubUser = {
+    "login": "FedMike",
+    "id": 98804214,
+    "avatar": "https://avatars.githubusercontent.com/u/98804214?v=4",
+    "name": "FedMike",
+    "company": "FedMike",
+    "blog": "",
+    "location": "FedMike",
+    "bio": "FedMike",
+    "twitter": "FedMike",
+    "repos": 35,
+    "followers": 1,
+    "following": 3,
+    "created": "2022-02-01T07:59:16Z",
+}
