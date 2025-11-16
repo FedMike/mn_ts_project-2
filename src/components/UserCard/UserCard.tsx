a475adb3d327019d72982d8fdd0a247c914405a1
fedmike <molnek87@mail.ru>
2025-11-16 15:45:21 +0900

    info component done

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -2,6 +2,7 @@ import { UserStat } from 'components/UserStat';
 import { LocalGithubUser } from 'types';
 import styles from './UserCard.module.scss';
 import { UserTitle } from 'components/UserTitle';
+import { UserInfo } from 'components/UserInfo';
 
 interface UserCardProps extends LocalGithubUser{ };
 
@@ -25,6 +26,12 @@ export const UserCard = (props: UserCardProps) => {
         repos={props.repos}
         followers={props.followers}
         following={props.following}
+      />
+      <UserInfo
+        blog={props.blog}
+        company={props.company}
+        location={props.location}
+        twitter={props.twitter}
       />
     </div>
   );
